33c7b629f2fe97b3422c3e1ba6f39b7a2399dd79
ihsankhan-752 <ihsankhan.se752@gmail.com>
2024-11-01 14:38:45 +0500

    Firebase Setup Done

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,17 @@
 import 'package:carnava_admin_panel/res/colors/app_colors.dart';
 import 'package:carnava_admin_panel/res/routes/routes.dart';
 import 'package:carnava_admin_panel/view/splash/splash_view.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get_navigation/src/root/get_material_app.dart';
 
-void main() {
+import 'firebase_options.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
